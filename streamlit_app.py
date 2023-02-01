15b61ce0086574c62cfc01a0cc61886f246c5ac5
Asen-Kratunkov <124148738+Asen-Kratunkov@users.noreply.github.com>
2023-02-01 12:52:29 +0200

    Update streamlit_app.py
    added new header with emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣Omega 3 & Bluebery Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭Build Your Own Fruit Smoothie🥝🍇')
